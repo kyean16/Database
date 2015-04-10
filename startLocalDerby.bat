6a1be40ca1bba0222a9b051adb87f1fac8a7c29d
keyean <kevinyean_2016@depauw.edu>
2015-04-10 18:39:34 -0400

    Added

---
+++ startLocalDerby.bat
@@ -0,0 +1,1 @@
+java -cp "\Program Files\java\jdk1.7.0_25\db\lib\derby.jar;\Program Files\java\jdk1.7.0_25\db\lib\derbynet.jar;\Program Files\java\jdk1.7.0_25\db\lib\derbyclient.jar;\Program Files\java\jdk1.7.0_25\db\lib\derbytools.jar" org.apache.derby.drda.NetworkServerControl start -h localhost -noSecurityManager
